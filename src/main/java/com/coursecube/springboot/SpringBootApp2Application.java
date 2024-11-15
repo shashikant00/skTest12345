5396d355a46e7ff18a9c1fabe6c575a6a0f55383
shashikant00 <shashikantgit10@gmail.com>
2024-11-15 23:30:11 +0530

    initial commit

---
+++ src/main/java/com/coursecube/springboot/SpringBootApp2Application.java
@@ -0,0 +1,13 @@
+package com.coursecube.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursecube/springboot/SpringBootApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursecube.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
