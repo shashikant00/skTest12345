311cc307f73497883158728c644f7987abccaaec
shashikant00 <shashikantgit10@gmail.com>
2024-11-15 23:49:03 +0530

    1st commit

--- src/main/java/com/coursecube/springboot/controller/RestHomeController.java
+++ src/main/java/com/coursecube/springboot/controller/RestHomeController.java
@@ -1,8 +1,13 @@
 package com.coursecube.springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestHomeController {
 
+	@GetMapping("/show")
+	public String showMasg() {
+		return "Show wish message";
+	}
 }
